ff01e92d46e3b160d37d218a5f0c1023f0af84a7
Tyler Muffly <tyler.muffly@dhha.org>
2020-02-09 19:14:51 -0700

    Got rid of Step_2_CK bug.

--- All_ERAS_data_merged.R
+++ All_ERAS_data_merged.R
@@ -35,61 +35,8 @@
     #arrange(desc(userid) %>%
     dplyr::select(userid, firstname, lastname, name) #, city_state, state)
   
-# 2019 ----
-  archive2019 <- exploratory::read_excel_file( "/Users/tylermuffly/Dropbox/Nomogram/nomogram/data/Archives/2019 All Data 1.xlsx", sheet = "642b9f43-7b4c-4ea1-8df7-00a8b42", na = c('','NA'), skip=0, col_names=TRUE, trim_ws=TRUE, col_types="text") %>%
-    readr::type_convert() %>%
-    exploratory::clean_data_frame() %>%
-    unite(`Applicant Name`, `First Name`, `Last Name`, sep = " ", remove = FALSE, na.rm = FALSE) %>%
-    dplyr::select(`AAMC ID`, `Applicant Name`, `Medical Education or Training Interrupted`, #ACLS, BLS, `Medical Licensure Problem`, PALS, Citizenship, `Sigma Sigma Phi`,
-                  `Malpractice Cases Pending`, `Felony Conviction`, `Alpha Omega Alpha`, `Date of Birth`, Gender, `Gold Humanism Honor Society`, `Military Service Obligation`, `Participating as a Couple in NRMP`, `Self Identify`,  `US or Canadian Applicant`, `Visa Sponsorship Needed`, #`Higher Education Degree`, 
-                  `Medical Degree`, `Medical School of Graduation`, `USMLE Step 1 Score`, `Tracks Applied by Applicant`, `Count of Non Peer Reviewed Online Publication`, `Count of Oral Presentation`, `Count of Other Articles`, `Count of Peer Reviewed Book Chapter`, `Count of Peer Reviewed Journal Articles/Abstracts`, `Count of Peer Reviewed Journal Articles/Abstracts(Other than Published)`, `Count of Peer Reviewed Online Publication`, `Count of Poster Presentation`, `Count of Scientific Monograph`, `Applicant Name`, `Misdemeanor Conviction`,
-                  
-                  #New variables to include for feature engineering.  Must be included for each year of data.   
-                  `Medical School Type`, `USMLE Step 2 CK Score`, #`Language Fluency`, 
-                  #`Higher Education Degree_1`, `Higher Education Degree_2`
-    ) %>%
-    #dplyr::rename(`Applicant Name` = PERSONAL_31) %>%
-    dplyr::rename(`Step_2_CK` = `USMLE Step 2 CK Score`) %>%
-    dplyr::mutate(`US or Canadian Applicant` = factor(`US or Canadian Applicant`)) %>%
-    dplyr::mutate(Gender = dplyr::recode(Gender, Female = "Female"), Gender = dplyr::recode(Gender, Female = "Female", Male = "Male", `No Response` = "Female")) %>%
-    #dplyr::mutate(BLS = dplyr::recode(BLS, Yes = "Yes", .missing = "No")) %>%
-    dplyr::mutate(`Medical Education or Training Interrupted` = factor(`Medical Education or Training Interrupted`), `Malpractice Cases Pending` = factor(`Malpractice Cases Pending`),`Alpha Omega Alpha` = dplyr::recode(`Alpha Omega Alpha`, `Alpha Omega Alpha (AOA) elections held during senior year` = "Elections_Senior_Year", `Alpha Omega Alpha (Member of AOA)` = "Yes", `No Alpha Omega Alpha (AOA) chapter at my school` = "No"), `Alpha Omega Alpha` = dplyr::recode(`Alpha Omega Alpha`, , .missing = "No"), `Alpha Omega Alpha` = factor(`Alpha Omega Alpha`)) %>%
-    #filter(!is.na(`Limiting Factors`)) %>%
-    dplyr::mutate(`Visa Sponsorship Needed` = dplyr::recode(`Visa Sponsorship Needed`, No = "No", Yes = "Yes", .missing = "No")) %>%
-    dplyr::mutate(`Gold Humanism Honor Society` = dplyr::recode(`Gold Humanism Honor Society`, `Gold Humanism Honor Society (Member of GHHS)` = "Yes", .missing = "Not a Member")) %>%
-    dplyr::mutate(`Gold Humanism Honor Society` = factor(`Gold Humanism Honor Society`)) %>%
-    dplyr::mutate(`Gold Humanism Honor Society` = factor(`Gold Humanism Honor Society`), `Participating as a Couple in NRMP` = factor(`Participating as a Couple in NRMP`), `Visa Sponsorship Needed` = factor(`Visa Sponsorship Needed`), Match_Status = dplyr::recode(`Tracks Applied by Applicant`, `Ob-Gyn/Preliminary|1076220P0 (Preliminary)` = "Did not match")) %>%
-    reorder_cols(Match_Status) %>%
-    dplyr::mutate(Match_Status = dplyr::recode(Match_Status, `Obstetrics-Gynecology|1076220C0 (Categorical)` = "Matched"), `Gold Humanism Honor Society` = dplyr::recode(`Gold Humanism Honor Society`, `No Gold Humanism Honor Society (GHHS) chapter at my school` = "No", `Not a Member` = "Not_a_Member", Yes = "Yes")) %>%
-    dplyr::mutate(Match_Status = dplyr::recode(Match_Status, `Did not match` = "Did_Not_Match", Matched = "Matched")) %>%
-    dplyr::select(-`Tracks Applied by Applicant`) %>%
-    dplyr::mutate(`Alpha Omega Alpha` = dplyr::recode(`Alpha Omega Alpha`, `Elections in Senior year` = "Senior_Year_Elections", No = "No", `No chapter` = "No_Chapter", Yes = "Yes"),  `Self Identify` = dplyr::recode(`Self Identify`, White = "White", `White|Other: Arab-American` = "White", `White|Other: Guyanese` = "White", `White|Other: Lebanese-American` = "White", `White|Other: Middle Eastern` = "White", `White|Other: Middle-Eastern (Jewish)` = "White", `White|Other: Persian` = "White", `White|Other: Portuguese` = "White", `White|Other: Turkish` = "White", .default = "Not_White")) %>%
-    #filter(!is.na(`Self Identify`)) %>%
-    # dplyr::mutate(`Sigma Sigma Phi` = dplyr::recode(`Sigma Sigma Phi`, `No Sigma Sigma Phi (SSP) chapter at my school` = "No_Chapter", `Sigma Sigma Phi (Member of SSP)` = "Yes_Member", .default = "No")) %>%
-    #dplyr::select(-`Higher Education Degree`) %>%
-    dplyr::mutate(`Medical Degree` = dplyr::recode(`Medical Degree`, `B.A./M.D.` = "MD", D.O. = "DO", `B.S./M.D.` = "MD", `DO/MA` = "DO", `DO/MBA` = "DO", `DO/MPH` = "DO", M.D. = "MD", `M.D./M.B.A.` = "MD", `M.D./M.P.H.` = "MD", `M.D./Other` = "MD", `M.D./Ph.D.` = "MD", `M.S./M.D.` = "MD", M.B. = "MD", `M.B.,B.S.` = "MD", M.B.B.Ch. = "MD", M.B.B.Ch.B = "MD", M.B.Ch.B. = "MD", B.A.O. = "MD", M.C. = "MD", M.Med. = "MD")) %>%
-    filter(!is.na(`Medical Degree`)) %>%
-    dplyr::mutate(`Medical Degree` = factor(`Medical Degree`), `Self Identify` = factor(`Self Identify`), `Military Service Obligation` = factor(`Military Service Obligation`)) %>%
-    #dplyr::select(-`Felony Conviction`) %>%
-    #dplyr::mutate(`OBGYN Grade--10` = factor(`OBGYN Grade--10`)) %>%
-    #dplyr::select(-`OBGYN Grade--10`) %>%
-    dplyr::mutate(Year = 2019) %>%   ####Cuts out 777 people here  
-    dplyr::mutate(`Date of Birth` = lubridate::mdy(`Date of Birth`)) %>%
-    dplyr::rename(DOB = `Date of Birth`) %>%
-    dplyr::mutate(`Current date` = lubridate::mdy("01/01/2019")) %>%
-    dplyr::mutate(Age = `Current date`- DOB, Age = as.numeric(Age)/365) %>%
-    filter(!is.na(Age) & Age >= 24) %>%
-    dplyr::select(-`Current date`, - DOB) %>%
-    dplyr::mutate(Match_Status = factor(Match_Status)) %>%
-    #clean_names(case = "parsed") %>%
-    dplyr::mutate(Year = dplyr::recode(Year, `2017` = "2019")) #%>%
-    #dplyr::rename(`Medical Licensure Problem` = Medical_Licensure_Problem) %>%
-    #dplyr::rename(Type_of_medical_school = `Medical_School_Type`)
   
-  
-  
-  
-  # Steps to produce 2018_archive ----
+  # 2018_archive ----
   #2018 does pull in Medical_School_of_Graduation
   archive2018 <- exploratory::read_delim_file("/Users/tylermuffly/Dropbox/Nomogram/nomogram/data/Archives/2018_archive.csv" , ",", quote = "\"", skip = 0 , col_names = TRUE , na = c('','NA') , locale=readr::locale(encoding = "UTF-8", decimal_mark = ".", grouping_mark = "," ), trim_ws = TRUE , progress = FALSE) %>%
     readr::type_convert() %>%
@@ -190,7 +137,7 @@
     dplyr::rename(`Medical Licensure Problem` = Medical_Licensure_Problem)
   
   
-  # Steps to produce 2016_archive ----
+  # 2016_archive ----
   # archive2016$Medical_School_of_Graduation
   archive2016 <- exploratory::read_delim_file("/Users/tylermuffly/Dropbox/Nomogram/nomogram/data/Archives/2016_archive.csv" , ",", quote = "\"", skip = 0 , col_names = TRUE , na = c('','NA') , locale=readr::locale(encoding = "UTF-8", decimal_mark = "."), trim_ws = TRUE , progress = FALSE) %>%
     readr::type_convert() %>%
@@ -271,7 +218,7 @@
     dplyr::select(-Positions_offered)
   
   
-  # Steps to produce the 2015 archive  ----
+  # 2015_archive  ----
   #archive2015$Medical_School_of_Graduation
   archive2015 <- exploratory::read_delim_file("/Users/tylermuffly/Dropbox/Nomogram/nomogram/data/Archives/2015_archive.csv" , ",", quote = "\"", skip = 0 , col_names = TRUE , na = c('','NA') , locale=readr::locale(encoding = "UTF-8", decimal_mark = "."), trim_ws = TRUE , progress = FALSE) %>%
     readr::type_convert() %>%
@@ -366,7 +313,7 @@
   setdiff(colnamesarchive2017, colnamesarchive2018)
   
   
-  #Bring all years together ----
+# all years together ----
   all_years <- 
     bind_rows(archive2015, id_column_name = "ID", current_df_name = "all_years", force_data_type = TRUE) %>%
     bind_rows(archive2016, id_column_name = "ID", current_df_name = "all_years", force_data_type = TRUE) %>%
@@ -378,7 +325,7 @@
     
   #New variable of medical school type
     mutate(Type_of_medical_school = dplyr::recode(Type_of_medical_school, `International School,International School` = "International School", `International School,International School,International School` = "International School", `International School,International School,U.S. Public School,International School` = "International School", `International School,U.S. Private School` = "International School", `International School,U.S. Private School,U.S. Private School,U.S. Private School` = "International School", `U.S. Private School,International School` = "U.S. Private School", `U.S. Private School,International School,U.S. Private School,U.S. Private School` = "U.S. Private School", `U.S. Private School,International School,U.S. Public School` = "U.S. Private School", `U.S. Private School,U.S. Private School,International School,U.S. Public School,U.S. Private School` = "U.S. Private School", `U.S. Private School,U.S. Public School` = "U.S. Private School", `U.S. Public School,International School` = "U.S. Public School", `U.S. Public School,International School,International School,International School` = "U.S. Public School", `U.S. Public School,Osteopathic School` = "U.S. Public School", `U.S. Public School,U.S. Private School` = "U.S. Public School", `U.S. Public School,U.S. Public School` = "U.S. Public School",  `International School,U.S. Public School,International School,International School` = "International School")) %>%
-
+    dplyr::rename(USMLE_Step_2_CK_Score = Step_2_CK) %>%
 
     filter(!is.na(USMLE_Step_2_CK_Score)) %>%
     
@@ -434,7 +381,7 @@
     fill(ACLS, Age, Alpha_Omega_Alpha, BLS, Citizenship, Count_of_Non_Peer_Reviewed_Online_Publication, Count_of_Oral_Presentation, Count_of_Other_Articles, Count_of_Peer_Reviewed_Book_Chapter, Count_of_Peer_Reviewed_Journal_Articles_Abstracts, Count_of_Peer_Reviewed_Journal_Articles_Abstracts_Other_than_Published, Count_of_Peer_Reviewed_Online_Publication, Count_of_Poster_Presentation, Count_of_Scientific_Monograph, Couples_Match, Gender, Gold_Humanism_Honor_Society, Medical_Education_or_Training_Interrupted, Malpractice_Cases_Pending, Medical_Degree, Military_Service_Obligation, Misdemeanor_Conviction, PALS, Sigma_Sigma_Phi, US_or_Canadian_Applicant, USMLE_Step_1_Score, Visa_Sponsorship_Needed, white_non_white, Match_Status, .direction = "down") %>%
     dplyr::mutate(Count_of_Non_Peer_Reviewed_Online_Publication = parse_number(Count_of_Non_Peer_Reviewed_Online_Publication), Match_Status_1 = Match_Status) %>%
     unite(`USMLE_Step_2_CK_Score`, `Step_2_CK`, sep = "_", remove = FALSE) %>%
-    dplyr::select(-`Step_2_CK`) %>%
+    #dplyr::select(-`Step_2_CK`) %>%
     dplyr::select(-ID_new_new_new, -userid, - Match_Status_1) %>%
     #as.numeric(USMLE_Step_2_CK_Score) %>%
     dplyr::filter(Age < 100) %>%
@@ -443,7 +390,7 @@
     #base::droplevels(all_years$Match_Status) %>%
     dplyr::select(- Malpractice_Cases_Pending) %>% #zero variance feature
     dplyr::select(-Gold_Humanism_Honor_Society) %>%
-    reorder_cols(ACLS, Age, Alpha_Omega_Alpha, BLS, Citizenship, Count_of_Non_Peer_Reviewed_Online_Publication, Count_of_Oral_Presentation, Count_of_Other_Articles, Count_of_Peer_Reviewed_Book_Chapter, Count_of_Peer_Reviewed_Journal_Articles_Abstracts, Count_of_Peer_Reviewed_Journal_Articles_Abstracts_Other_than_Published, Count_of_Peer_Reviewed_Online_Publication, Count_of_Poster_Presentation, Count_of_Scientific_Monograph, Couples_Match, Gender, Medical_Education_or_Training_Interrupted, Medical_Degree, Military_Service_Obligation, Misdemeanor_Conviction, PALS, Sigma_Sigma_Phi, US_or_Canadian_Applicant, USMLE_Step_1_Score, Visa_Sponsorship_Needed, white_non_white, Medical_Licensure_Problem, Medical_School_of_Graduation, Type_of_medical_school, USMLE_Step_2_CK_Score, NIH_dollars, Match_Status)
+    reorder_cols(ACLS, Age, Alpha_Omega_Alpha, BLS, Citizenship, Count_of_Non_Peer_Reviewed_Online_Publication, Count_of_Oral_Presentation, Count_of_Other_Articles, Count_of_Peer_Reviewed_Book_Chapter, Count_of_Peer_Reviewed_Journal_Articles_Abstracts, Count_of_Peer_Reviewed_Journal_Articles_Abstracts_Other_than_Published, Count_of_Peer_Reviewed_Online_Publication, Count_of_Poster_Presentation, Count_of_Scientific_Monograph, Couples_Match, Gender, Medical_Education_or_Training_Interrupted, Medical_Degree, Military_Service_Obligation, Misdemeanor_Conviction, PALS, Sigma_Sigma_Phi, US_or_Canadian_Applicant, USMLE_Step_1_Score, Visa_Sponsorship_Needed, white_non_white, Medical_Licensure_Problem, Medical_School_of_Graduation, Type_of_medical_school, NIH_dollars, USMLE_Step_2_CK_Score, Match_Status) %>%
   
   colnames(all_years)
   dim(all_years)
@@ -500,7 +447,7 @@
   view(summarytools::dfSummary(x = all_years, justify = "l", style = "multiline", varnumbers = FALSE, valid.col = FALSE, tmp.img.dir = "./img", max.distinct.values = 5))
   
   write_csv(x = all_years, path = "~/Dropbox/Nomogram/nomogram/data/All_ERAS_data_merged_output_2_1_2020.csv")
-  
+
   rm(archive2015)
   rm(archive2016)
   rm(archive2017)
