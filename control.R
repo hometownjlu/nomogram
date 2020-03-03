cfc656dec61acf1080edad3eec01b4576ed2fd36
Tyler Muffly <tyler.muffly@dhha.org>
2020-03-03 07:55:57 -0700

    I removed Step 1 given that it will go pass fail.

--- control.R
+++ control.R
@@ -10,25 +10,30 @@ source("~/Dropbox/Nomogram/nomogram/Code/Additional_functions_nomogram.R", echo=
 
 #Exploratory Data Analysis
 #rmarkdown::render()
-rmarkdown::render(input = "~/Dropbox/Nomogram/nomogram/00-Exploratory_Data_Analysis.Rmd")
-pander::openFileInOS("00-Exploratory_Data_Analysis.html")
-pander::openFileInOS("00-Exploratory_Data_Analysis.pdf")
+rmarkdown::render(input = "~/Dropbox/Nomogram/nomogram/00-Exploratory_Data_Analysis.Rmd", output_format = c("html_document", "pdf_document"), clean = TRUE)
+pander::openFileInOS("~/Dropbox/Nomogram/nomogram/00-Exploratory_Data_Analysis.html")
+pander::openFileInOS("~/Dropbox/Nomogram/nomogram/00-Exploratory_Data_Analysis.pdf")
+
 
 #Split the Data
 #rmarkdown::render()
-rmarkdown::render(input = "~/Dropbox/Nomogram/nomogram/01-Split_Data.Rmd")
+rmarkdown::render(input = "~/Dropbox/Nomogram/nomogram/01-Split_Data.Rmd", output_format = c("html_document", "pdf_document"), clean = TRUE)
 pander::openFileInOS("01-Split_Data.html")
-
+pander::openFileInOS("01-Split_Data.pdf")
 
 #Kitchen Sink Model
 #rmarkdown::render()
-rmarkdown::render(input = "~/Dropbox/Nomogram/nomogram/02-Kitchen_Sink_Model.Rmd")
+rmarkdown::render(input = "~/Dropbox/Nomogram/nomogram/02-Kitchen_Sink_Model.Rmd", output_format = c("html_document", "pdf_document"), clean = TRUE)
 pander::openFileInOS("02-Kitchen_Sink_Model.html")
+pander::openFileInOS("02-Kitchen_Sink_Model.pdf")
 
 #Feature Selection
-rmarkdown::render(input = "~/Dropbox/Nomogram/nomogram/03-Feature_Selection.Rmd")
+rmarkdown::render(input = "~/Dropbox/Nomogram/nomogram/03-Feature_Selection.Rmd", output_format = c("html_document", "pdf_document"), clean = TRUE)
 pander::openFileInOS("03-Feature_Selection.html")
+pander::openFileInOS("03-Feature_Selection.pdf")
 
-#Kitchen Sink Model
-rmarkdown::render(input = "~/Dropbox/Nomogram/nomogram/04-GLM_and_CART_models.Rmd")
+#GLM_and_CART_models
+rmarkdown::render(input = "~/Dropbox/Nomogram/nomogram/04-GLM_and_CART_models.Rmd", output_format = c("html_document", "pdf_document"), clean = TRUE)
 pander::openFileInOS("04-GLM_and_CART_models.html")
+pander::openFileInOS("04-GLM_and_CART_models.pdf")
+
