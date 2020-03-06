9073c723fa3662965ca411e9d6b2b86f9013a558
Tyler Muffly <tyler.muffly@dhha.org>
2020-03-05 21:00:12 -0700

    Trying to get all packages working.

--- Code/Additional_functions_nomogram.R
+++ Code/Additional_functions_nomogram.R
@@ -26,6 +26,38 @@ doMC::registerDoMC(cores = detectCores()-1) #Use multiple cores for processing
 #brew install pandoc
 #brew install pkg-config
 
+install.packages("officer")
+install.packages("rvg")
+install.packages("openxlsx")
+install.packages("ggplot2")
+install.packages("flextable")
+install.packages("xtable")
+install.packages("rgl")
+install.packages("stargazer")
+install.packages("tikzDevice")
+install.packages("xml2")
+install.packages("broom")
+install.packages("devtools")
+library(devtools)
+devtools::install_github("tomwenseleers/export")
+
+# The following two commands remove any previously installed H2O packages for R.
+if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+
+# Next, we download packages that H2O depends on.
+pkgs <- c("RCurl","jsonlite")
+for (pkg in pkgs) {
+  if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
+}
+
+# Now we download, install and initialize the H2O package for R.
+install.packages("h2o", type="source", repos="http://h2o-release.s3.amazonaws.com/h2o/rel-yu/2/R")
+
+# Finally, let's load H2O and start up an H2O cluster
+library(h2o)
+h2o.init()
+
 
 #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### 
 # Directory Paths for Data and Results
